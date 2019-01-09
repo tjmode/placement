c950c178f666df26515ea7a1b8ebaab803f10374
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-09 22:27:45 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,9 @@
+ch=(input())
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Vowel")
+elif ((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    print("consonant")
+else:
+    print("invalid")
+#tony.jpg
