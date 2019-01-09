9e456ced5dac2693be0018487bdb23a2fdf57508
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-09 23:08:34 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -2,10 +2,10 @@ year=int(input())
 if (year % 4) == 0:
    if (year % 100) == 0:
        if (year % 400) == 0:
-           print("Yes")
+           print("yes")
        else:
            print("no")
    else:
        print("yes")
 else:
-   print("No")
+   print("no")
