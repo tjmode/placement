e75689eaf91402f211c0d23a1a190af1dd435c87
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-10 23:25:39 +0530

    Rename max.py to max2.py

---
+++ max2.py
@@ -0,0 +1,9 @@
+number_array = list()
+number = input()
+for i in range(int(number)):
+    n = input()
+    number_array.append(int(n))
+print (number_array)
+number_array.sort()
+i=len(number_array)-1
+print(number_array[i])
