d74f71523c7fc62d877e89cd4c5a1a1634302b02
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-10 23:32:05 +0530

    Update max2.py

--- max2.py
+++ max2.py
@@ -3,6 +3,4 @@ number = input()
 for i in range(int(number)):
     n = input()
     number_array.append(int(n))
-number_array.sort()
-i=len(number_array)-1
-print(number_array[i])
+print(max(number_array))
