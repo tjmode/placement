cc4ec8e486fb2d88d28c0e0a2b8585f81c7fa423
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-10 20:09:05 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,11 @@
+#primetony
+num = int(input())
+if num <=1000:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+           break
+   else:
+       print("yes")
+else:
+   print("no")
