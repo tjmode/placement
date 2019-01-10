c012a985f99a5809871090683c14176b9d642f34
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-10 21:36:07 +0530

    Create primerange.py

---
+++ primerange.py
@@ -0,0 +1,9 @@
+f,s= [int(x) for x in input().split()]  
+for num in range(f,s+1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print(num,'\n')
+           #tont
