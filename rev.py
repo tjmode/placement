eca6ae9d965e4ac0df24e81b34b4d16e0b09a5c5
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-10 20:02:47 +0530

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,8 +1,11 @@
-x=int(input())
-str(x)
-b=str(x)[::-1]
-print(b)
-if x==b:
+n=int(input())
+temp=n
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+if(temp==rev):
     print("yes")
-else
+else:
     print("no")
