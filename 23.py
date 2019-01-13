b03592693fa8632b2dc7dd8047077b7e6b9c5f61
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-13 20:01:49 +0530

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,8 +1,8 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=sorted(a)
-for i in range(0,len(a)):
-    if(i==len(a)):
+for i in range(0,len(b)):
+    if(i==len(b)-1):
         print(b[i],end="")
     else:
         print(b[i],end=" ")
