0ad11319bb1bf6c7ab9050adf4afd5db6bddc4ca
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-15 23:36:48 +0530

    Update sameline.py

--- sameline.py
+++ sameline.py
@@ -1,8 +1,13 @@
 x=int(input())
 n=0
 b=(str(x)[::-1])
+c=len(b)
 a=int(b)
 while a>n:
     s=a%10
-    print(s,end=" ")
+    n=n+1
+    if n==c:
+        print(s,end="")
+    else:
+        print(s,end=" ")
     a=a//10
