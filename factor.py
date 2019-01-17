860b043a8a4a34014cf91f174618e1daa291e049
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-17 17:01:54 +0530

    Create factor.py

---
+++ factor.py
@@ -0,0 +1,9 @@
+num=int(input("enter a number"))
+
+for i in range(1,num+1):
+    if num%i==0:
+        if i==num:
+            
+           print(i,end="")
+        else:
+            print(i,end=" ")
