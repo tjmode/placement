20356de03446653e9c110dff85e33f0e5b89a0db
tjmode <40241323+tjmode@users.noreply.github.com>
2019-01-17 16:35:36 +0530

    Create composite.py

---
+++ composite.py
@@ -0,0 +1,12 @@
+num = int(input())
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print("yes")
+            break
+    else:
+        print("no")
+elif num == 0 or 1:
+    print("no")
+else:
+    print("yes")
