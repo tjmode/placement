1d22477f0d8513ce13a352b6ed6218d99fcda939
tjmode <40241323+tjmode@users.noreply.github.com>
2019-02-18 21:17:39 +0530

    Update 53-pro.py

--- 53-pro.py
+++ 53-pro.py
@@ -1,12 +1,14 @@
 s=input()
 a=[]
+j1=0
 d=s.replace(" ","")
 d1=d.lower()
 for i in d1:
     if i not in a:
         a.append(i)
-    
-if len(a)==26:
+for j in a:
+    j1=j1+1
+if j1==26:
     print("yes")
 else:
     print("no")
