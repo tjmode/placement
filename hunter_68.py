7aa2c70eec7b254d617d7db219651fef108c9a0d
tony wilson <40241323+tjmode@users.noreply.github.com>
2019-02-26 22:20:26 +0530

    Create hunter_68.py

---
+++ hunter_68.py
@@ -0,0 +1,21 @@
+s=list(map(int,input().split()))
+d=[]
+f=[]
+g=[]
+p=0
+o=0
+for i in s:
+    d.append(i)
+    f.append(i)
+f.sort()
+for i in d:
+    p=p+1
+    if i==f[0]:
+        g.append(p)
+
+for j in d:
+    o=o+1
+    if j==f[len(f)-1]:
+        g.append(o)
+print(g[0],g[1])
+#dude_devs
