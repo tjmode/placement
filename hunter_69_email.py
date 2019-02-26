9c880fe2d402eed03109088939d627bf92ad0c17
tony wilson <40241323+tjmode@users.noreply.github.com>
2019-02-26 21:49:35 +0530

    Create hunter_69_email.py

---
+++ hunter_69_email.py
@@ -0,0 +1,23 @@
+p=0
+a=0
+q=0
+w=0
+e=0
+s=input()
+d=s.split('@')
+d2=d[1].split(".")
+if len(d[0])>=3 :
+       p=p+1
+if len(d2[0])>=4:
+    a=a+1
+if d2[1]=="com":
+    e=e+1
+for i in s:
+    if i=="@":
+        q=q+1
+    elif i==".":
+        w=w+1
+if (p==1 and a==1 and q==1 and w==1 and e==1):
+    print("YES")
+else:
+    print("NO")
